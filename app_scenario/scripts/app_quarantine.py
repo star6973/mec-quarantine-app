c30c97aacb0b47393a7ac14d849d9c8a63306912
MH.Ji <battlesun99@gmail.com>
2021-07-01 19:33:46 +0900

    revise qpp_quarantine.py

--- app_scenario/scripts/app_quarantine.py
+++ app_scenario/scripts/app_quarantine.py
@@ -21,84 +21,89 @@ from sero_actions.msg import *
 
 class MyLoop(Loop):
     def on_create(self, event):
-        self.logger.info("$$$ create quarantine!!!")
-
         self.add_listener(self.make_node("{namespace}/quarantine/ui_ready"), self.on_front_ui_ready)
-        # self.add_listener(self.make_node("{namespace}/quarantine/ui_finish"), self.)
-        # self.add_listener(self.make_node("{namespace}/workerbee_navigation/robot_pose"), self.)
-        # self.add_listener(self.make_node("{namespace}/sero_mobile/lpt"), self.)
-        # self.add_listener(self.make_node("{namespace}/sero_mobile/battery"), self.)
+        self.add_listener(self.make_node("{namespace}/quarantine/ui_finish"), self.on_front_ui_finish)
 
         return ResponseInfo()
     
     def on_resume(self, event):
-        self.logger.info("$$$ resume quarantine!!!")
         self.publish(self.make_node("{namespace}/robot_display/open_url"), {"content": "http://0.0.0.0:8080/app_quarantine.html?" + urllib.urlencode(event)})
 
-        self.robot_pose = Pose()
+        '''
+            Read the required yaml file
+        '''
         self.location_doc = self.load_document("quarantine_location")
+        self.preference_doc = self.load_document("preferences")
 
-        self.logger.info("location_doc = ", self.location_doc)
-
+        '''
+            Parse info from app_event file
+        '''
         self.schedule_end_time = dateutil.parser.parse(event["end_time"])
         self.target_loc = event["location"]
 
-        self.logger.info("schedule_end_time = ", self.schedule_end_time, type(self.schedule_end_time))
-        self.logger.info("target_loc = ", self.target_loc, type(self.target_loc))
+        '''
+            Parse info from quarantine_location.yaml
+        '''
+        self.poi_list = self.get_poi_list_with_target_loc()
 
-        self.speed = None
-        self.disable_global_path_planning = False
+        '''
+            Parse info from preference.yaml
+        '''
+        self.speed = self.preference_doc["SPEED"]
+        self.disable_global_path_planning = self.preference_doc["DISABLE_GLOBAL_PATH_PLANNING"]
+        self.TRY_LPT_COUNT = self.preference_doc["TRY_LPT_COUNT"]
+        self.TRY_DRIVE_COUNT = self.preference_doc["TRY_DRIVE_COUNT"]
+
+        '''
+            Flags for running the service
+        '''
         self.finish_quarantine_flag = False
         self.finish_drive_flag = False
         self.finish_lpt_flag = False
-        self.poi_list = self.get_poi_list_with_target_loc()
-        self.logger.info("poi list with target location = ", self.poi_list)
+        
 
+        '''
+            Flags for others
+        '''
         self.poi_idx = 0
+        self.try_lpt_count = 0
         self.try_drive_count = 0
-        self.TRY_DRIVE_COUNT = 5
-
         self.front_ui_ready = False
+
         # javascript로 구성된 front-end단이 python으로 구성된 back-end단의 실행 시간보다 빠르기 때문에 sleep을 걸어준다.
         while self.front_ui_ready == False:
             time.sleep(0.5)
 
         return ResponseInfo()
 
+    '''
+        [Scenario]
+        1. schedule_end_time 범위 안에서
+            1-1. docking station에서 undocking하기
+            1-2. location.yaml에 있는 lpt 위치로 설정하기
+            1-3. location.yaml에 있는 poi 위치로 이동하기
+            1-4. quarantine.js UI로 호출하기
+        2. schedule_end_time 범위 밖에서
+            2-1. app_event로 전환하기
+    '''
     def on_loop(self):
-        '''
-            [Scenario]
-            1. app_event.py에서 schedule_end_time을 받아온다.
-            2. docking station에서 undocking된 후, quarantine.js로 UI 시작을 호출한다.
-            3. schedule_end_time이 끝나기 전까지, location.yaml에 있는 poi 위치들을 반복해서 이동한다.
-            4. schedule_end_time이 끝나면 charging 시나리오로 전환한다.
-        '''
-        self.logger.info("### loop quarantine!!!")
-
         if self.finish_quarantine_flag == False:
-            self.logger.info(datetime.datetime.now())
-            # 현재 시간이 schedule_end_time을 넘어갈 경우 quarantine 모듈 종료
             if self.schedule_end_time < datetime.datetime.now():
-                self.logger.info("현재 시간이 schedule_end_time을 넘어갈 경우")
                 pass
 
             else:
-                self.logger.info("Start QUARANTINE!!!!!!!!!!!")
                 if self.finish_lpt_flag == False:
-                    self.set_to_lpt()
+                    if self.try_lpt_count < self.TRY_LPT_COUNT:
+                        self.action_lpt()
+                    else:
+                        self.logger.warning("LPT 액션 수행 실패")
+                        self.finish_lpt_flag = True
 
                 if self.finish_drive_flag == False:
-                    
-                    self.logger.info("poi idx = ", self.poi_idx)
-
-                    # if self.front_ui_ready == False:
-                    #     self.pub_ui()
-
                     if self.try_drive_count < self.TRY_DRIVE_COUNT:
-                        self.logger.info("Start Drive poi!!!!")
-                        self.move_to_poi()
-
+                        self.action_driving()
                     else:
+                        self.logger.warning("DRIVING 액션 수행 실패")
                         self.finish_quarantine_flag = True
 
         return ResponseInfo()
@@ -109,9 +114,9 @@ class MyLoop(Loop):
     def on_destroy(self, event):
         return ResponseInfo()
 
+    # quarantine.js에서 ui가 준비되었음을 알리는 콜백 함수
     def on_front_ui_ready(self, msg):
         self.front_ui_ready = True
-
 
     def get_poi_list_with_target_loc(self):
         for doc in self.location_doc:
@@ -121,103 +126,99 @@ class MyLoop(Loop):
             if name == self.target_loc:
                 return poi
 
-        self.logger.info("Target Location과 일치하는 poi 값이 없음")
-        # error
+        self.logger.warning("Target Location과 일치하는 poi 값이 없음")
+        return []
 
-    def set_to_lpt(self):
-        target_poi = self.poi_list[self.poi_idx]
-        target_lift = target_poi["lpt"]["lift"]
-        target_pan = target_poi["lpt"]["pan"]
-        target_tilt = target_poi["lpt"]["tilt"]
+    def action_lpt(self):
+        try:
+            target_poi = self.poi_list[self.poi_idx]
+            target_lift = target_poi["lpt"]["lift"]
+            target_pan = target_poi["lpt"]["pan"]
+            target_tilt = target_poi["lpt"]["tilt"]
 
-        self.logger.info("\n\n @@@@@ 타겟 LPT 값들 = {}, {}, {}\n".format(target_lift, target_pan, target_tilt))
+            # target lpt값으로 액션 명령어 수행
+            self.action_sync(
+                self.make_node("{namespace}/sero_mobile/lpt_set_position"),
+                {
+                    "lift": target_lift,
+                    "pan": target_pan,
+                    "tilt": target_tilt,
+                },
+            )
+            self.finish_lpt_flag = True
+        
+        except Exception as e:
+            self.logger.warning("LPT 액션 수행에서 문제 발생 = {}".format(e))
+            self.try_lpt_count += 1
 
-        # target lpt값으로 액션 명령어 수행
-        self.action_sync(
-            self.make_node("{namespace}/sero_mobile/lpt_set_position"),
-            {
-                "lift": target_lift,
-                "pan": target_pan,
-                "tilt": target_tilt,
-            },
-        )
+    def action_driving(self):
+        try:
+            if self.poi_idx >= len(self.poi_list):
+                self.poi_idx = 0
 
-        self.finish_lpt_flag = True
+            pose = Pose()
+            target_poi = self.poi_list[self.poi_idx]
+            pose.position = Point(x=target_poi["pose"]["x"], y=target_poi["pose"]["y"], z=target_poi["pose"]["z"])
+            pose.orientation.x = target_poi["orientation"]["x"]
+            pose.orientation.y = target_poi["orientation"]["y"]
+            pose.orientation.z = target_poi["orientation"]["z"]
+            pose.orientation.w = target_poi["orientation"]["w"]
 
-    def move_to_poi(self):
+            # MoveToActionGoal Msg
+            msg = MoveToActionGoal()
+            msg.goal.goal.header.frame_id = "map"
+            msg.goal.goal.pose = pose
+            msg.goal.speed = self.speed
+            msg.goal.disable_global_path_planning = self.disable_global_path_planning
+            msg.goal.patience_timeout = 30.0
+            msg.goal.disable_obstacle_avoidance = False
+            msg.goal.endless = False
 
-        if self.poi_idx >= len(self.poi_list):
-            self.logger.info("All poi Finish!!")
-            self.finish_quarantine_flag = True
-            return
+            # moveto action node
+            action_node = self.make_node("{namespace}/workerbee_navigation/moveto")
+            gen = self.action_generate(action_node, msg, timeout=30.0, auto_cancel=True)
 
-        pose = Pose()
+            # if action start, on_loop does not activate
+            for process in gen:
+                driver_state_code = process.body["state"]["driver_state"]["code"]
+                action_state_code = process.body["state"]["action_state"]["code"]
+                if driver_state_code > 1 or action_state_code != ActionState.NO_ERROR:
+                    pass
 
-        target_poi = self.poi_list[self.poi_idx]
-        pose.position = Point(x=target_poi["pose"]["x"], y=target_poi["pose"]["y"], z=target_poi["pose"]["z"])
-        pose.orientation.x = target_poi["orientation"]["x"]
-        pose.orientation.y = target_poi["orientation"]["y"]
-        pose.orientation.z = target_poi["orientation"]["z"]
-        pose.orientation.w = target_poi["orientation"]["w"]
+            self.logger.info("Action Generate Result = {}\n".format(gen.result))
 
-        # MoveToActionGoal Msg
-        msg = MoveToActionGoal()
-        msg.goal.goal.header.frame_id = "map"
-        msg.goal.goal.pose = pose
-        msg.goal.speed = 0.2
-        msg.goal.disable_global_path_planning = False
-        msg.goal.patience_timeout = 30.0
-        msg.goal.disable_obstacle_avoidance = False
-        msg.goal.endless = False
+            # /workerbee_navigation/moveto/result 토픽 결과를 바탕으로 action 성공 유무 확인
+            if gen.result.error == False:
+                driver_state_code = gen.result.body["state"]["driver_state"]["code"]
+                action_state_code = gen.result.body["state"]["action_state"]["code"]
 
-        # moveto action node
-        action_node = self.make_node("{namespace}/workerbee_navigation/moveto")
+                # 아래 조건들은 특수한 경우의 예외 조건
+                if driver_state_code == MoveToDriverState.ERROR_PLANNER and action_state_code == ActionState.ERROR_DRIVER:
+                    self.logger.warning("ERROR PLANNING\n")
+                    self.try_drive_count += 1
+                    time.sleep(2)
 
-        gen = self.action_generate(action_node, msg, timeout=30.0, auto_cancel=True)
+                elif driver_state_code != MoveToDriverState.NO_ERROR or action_state_code != ActionState.NO_ERROR:
+                    self.try_drive_count += 1
+                    time.sleep(2)
 
-        # doing action loop not activate
-        for process in gen:
-            driver_state_code = process.body["state"]["driver_state"]["code"]
-            action_state_code = process.body["state"]["action_state"]["code"]
-            if driver_state_code > 1 or action_state_code != ActionState.NO_ERROR:
-                pass
-
-        self.logger.info("\n\n @@@@@ Action Generate Result = {}\n".format(gen.result))
-
-        # /workerbee_navigation/moveto/result 토픽 결과를 바탕으로 action 성공 유무 확인
-        if gen.result.error == False:
-            driver_state_code = gen.result.body["state"]["driver_state"]["code"]
-            action_state_code = gen.result.body["state"]["action_state"]["code"]
-
-            # 아래 조건들은 특수한 경우의 예외 조건
-            if driver_state_code == MoveToDriverState.ERROR_PLANNER and action_state_code == ActionState.ERROR_DRIVER:
-                self.logger.error("\n\n @@@@@ ERROR PLANNING \n")
-                self.finish_drive_flag = False
-                self.try_drive_count += 1
-                time.sleep(2)
-
-            elif driver_state_code != MoveToDriverState.NO_ERROR or action_state_code != ActionState.NO_ERROR:
-                self.finish_drive_flag = False
-                self.try_drive_count += 1
-                time.sleep(2)
+                else:
+                    self.logger.info("Move to POI Success\n")
+                    self.poi_idx += 1
+                    self.try_drive_count = 0
 
             else:
-                self.logger.info("\n\n @@@@@ Action Generate Result No Error \n")
-                self.finish_drive_flag = False
-                self.try_drive_count = 0
-                self.poi_idx += 1
+                driver_state_code = gen.result.body["state"]["driver_state"]["code"]
+                action_state_code = gen.result.body["state"]["action_state"]["code"]
+                if driver_state_code == MoveToDriverState.NO_ERROR and action_state_code == ActionState.ERROR_FAULT:
+                    self.logger.warning("LOCALIZATION FAULT\n")
 
-        # ERROR
-        else:
-            driver_state_code = gen.result.body["state"]["driver_state"]["code"]
-            action_state_code = gen.result.body["state"]["action_state"]["code"]
-            if driver_state_code == MoveToDriverState.NO_ERROR and action_state_code == ActionState.ERROR_FAULT:
-                self.logger.error("\n\n @@@@@ LOCALIZATION FAULT \n")
+                self.logger.warning("ERROR DRIVER\n")
+                self.try_drive_count += 1
 
-            self.logger.error("\n\n @@@@@ ERROR DRIVER \n")
-            self.finish_drive_flag = False
+        except Exception as e:
+            self.logger.warning("DRIVING 액션 수행에서 문제 발생 = {}".format(e))
             self.try_drive_count += 1
-
 
 __class = MyLoop
 DOCUMENT_DIR = os.path.expanduser("~") + "/document/"
