5fd2f5ef0d0927a45b94ea6dfbccde00e9e743a8
MH.Ji <battlesun99@gmail.com>
2021-07-22 11:06:11 +0900

    preferences.yaml 파일 모드 변경 오류 Fixes #5 console.js에서 initializing 함수 확인

--- app_ui/src/js/console.js
+++ app_ui/src/js/console.js
@@ -1,3 +1,7 @@
+const SECURE_MODE = "secure"
+const INSPECTION_MODE = "inspection"
+const QUARANTINE_MODE = "quarantine"
+
 app.controller("BodyCtrl", function ($scope, $http) {
     bodyScope = $scope
     $scope.params_complete = false
@@ -15,7 +19,7 @@ app.controller("BodyCtrl", function ($scope, $http) {
     }
 
     $scope.ui_initialize = function () {
-        $scope.view_mode = "secure"
+        $scope.view_mode = SECURE_MODE
     }
 
     $scope.ui_initialize()
@@ -30,4 +34,42 @@ app.controller("BodyCtrl", function ($scope, $http) {
             $scope.Initializing()
         }
     }
+
+    $scope.Initializing(() => {
+        $.getJSON("../document/preferences")
+            .then((doc) => {
+                if (doc["MODE"] == INSPECTION_MODE) {
+                    $scope.service_mode = INSPECTION_MODE
+                } else {
+                    $scope.service_mode = QUARANTINE_MODE
+                }
+            })
+    })
+
+    /*
+        handle for immediate schedule
+    */
+    $scope.imm_sch_cancel = true
+    $scope.imm_sch_hour = "00"
+    $scope.imm_sch_min = "00"
+
+    $.getJSON("../document/immediate_schedule")
+        .them((doc) => {
+            let hour = "" + new Date().getHours();
+            let minute = "" + new Date().getMinutes();
+            
+            let imm_time = $scope.imm_sch_hour + $scope.imm_sch_min
+            let cur_time = hour + minute
+
+            if (cur_time > imm_time) {
+                $scope.imm_sch_cancel = true
+                $scope.imm_sch_hour = "00"
+                $scope.imm_sch_min = "00"
+            } else {
+                $scope.imm_sch_cancel = msg.IMMEDIATE_SCHEDULE_CANCELED
+                $scope.imm_sch_hour = msg.IMMEDIATE_SCHEDULE_HOUR
+                $scope.imm_sch_min = msg.IMMEDIATE_SCHEDULE_MINUTE
+            }
+
+            safeApply($scope, function (bodyScope) {})
 });
