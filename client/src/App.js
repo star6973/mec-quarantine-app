4559055f9bef0ee8fa5c45be0e292aedb3402799
mhji <battlesun99@gmail.com>
2021-08-06 23:04:51 +0900

    Feat: create react app and add inspection function

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import Inspection from "./Inspection";
 
 function App() {
   return (
     <div className="App">
+      <div className="app_controller">
+        <div className="inspection_controller">
+          <h1>Inspection Mode</h1>
+          <Inspection />
+        </div>
+        <div className="quarantine_controller">
+          <h1>Quarantine Mode</h1>
+        </div>
+      </div>
     </div>
   );
 }
