87ae81f59348083e8937f9b1090c97e5b581e384
mhji <battlesun99@gmail.com>
2021-08-12 14:55:00 +0900

    Refactor: refactoring react style component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
 import Inspection from "./Inspection";
+import Quarantine from './Quarantine';
 
 function App() {
   return (
     <div className="App">
-      <div className="app_controller">
-        <div className="inspection_controller">
-          <h1>Inspection Mode</h1>
-          <Inspection />
-        </div>
-        <div className="quarantine_controller">
-          <h1>Quarantine Mode</h1>
-        </div>
-      </div>
+      <Quarantine />
     </div>
   );
 }
