db7e53071012706e1b0d49b8230a9723ef36f5e7
mhji <battlesun99@gmail.com>
2021-08-12 14:54:01 +0900

    Feat: create image list component

---
+++ client/src/Image.js
@@ -0,0 +1,23 @@
+import Moving from "./img/lottie/moving.json"
+import TemperatureSmall from "./img/temperature-small.png";
+import TemperatureLarge from "./img/temperature-large.png";
+import MaskSmall from "./img/mask-small.png";
+import MaskLarge from "./img/mask-large.png";
+import DistanceSmall from "./img/distance-small.png";
+import DistanceLarge from "./img/distance-large.png";
+import Protect0 from "./img/protect-0.png";
+import Protect1 from "./img/protect-1.jpg";
+import Protect2 from "./img/protect-2.jpg";
+
+export {
+    Moving,
+    TemperatureSmall,
+    TemperatureLarge,
+    MaskSmall,
+    MaskLarge,
+    DistanceSmall,
+    DistanceLarge,
+    Protect0,
+    Protect1,
+    Protect2
+};
