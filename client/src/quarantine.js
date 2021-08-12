e058531f8c93e88f13946cfb33e35ad400aa942c
mhji <battlesun99@gmail.com>
2021-08-12 14:54:30 +0900

    Refactor: refactoring react style component

--- client/src/quarantine.js
+++ client/src/quarantine.js
@@ -1,127 +1,47 @@
-const STATE_MOVING = 'moving';
-const STATE_QUARANTINE = 'quarantine';
-let image_and_sound_dict = {}
-let ui_apply = null;
-let interval = null;
-var idx = 0;
+import "./Quarantine.css";
+import * as Image from "./Image";
+import { useEffect, useState } from "react";
+import { LottieControl } from "./Common";
 
-app.controller("BodyCtrl", function ($scope, $http) {
-    bodyScope = $scope;
-    $scope.params_complete = false
-    $scope.nats_complete = false
+function ChangeImage({timeout}) {
+    const [state, setState] = useState(Image.Protect0);
 
-    $scope.getLang = (key) => {
-        if ($scope.lang_v2 != undefined && key != undefined) {
-            if ($scope.lang_v2[key]) {
-                return $scope.lang_v2[key][$scope.lang_num];
-            } else
-                console.warn(key);
-        } else {
-            return '';
+    useEffect (() => {
+        const pageList = [Image.Protect0, Image.Protect1, Image.Protect2];
+        let idx = 1;
+        const intervalID = setInterval(() => {
+            setState(pageList[idx])
+            idx = (idx + 1) % pageList.length;
+        }, timeout);
+
+        return () => {
+            clearInterval(intervalID);
         }
-    }
+    }, [])
 
-    $scope.ui_initialize = function () {
-        $scope.quarantine_state = STATE_MOVING;
-        lottieloader('quarantine_mv_lottie', './contents/img/lottie/move/data.json', true, true);
-    }
+    return (
+        <img src={state} alt="quarantine_img" />
+    )
+}
 
-    $scope.ui_initialize()
+function Quarantine() {
+    const [state, setState] = useState("quaranting");
 
-    $scope.check_initialized = (msg) => {
-        if (msg == 'params')
-            $scope.params_complete = true
-        else if (msg == 'nats')
-            $scope.nats_complete = true
-
-        if ($scope.params_complete && $scope.nats_complete) {
-            $scope.Initializing()
-        }
-    }
-
-    $scope.Initializing = () => {
-        nats.addListener(namespace + '/robot_scenario/quarantine_start', $scope.onStartQuarantine);
-        nats.addListener(namespace + '/robot_scenario/quarantine_finish', $scope.onEndQuarantine);
-        nats.publish(namespace + '/quarantine/ui_ready', {});
-
-        $.getJSON('../document/image_speak_item_box')
-        .then((img) => {
-            for (let i=0; i<img.length; i++) {
-                let use = Object.values(img)[i]["use"];
-                let tmp = {};
-                if (use == true) {
-                    tmp["image"] = Object.values(img)[i]["image"];
-                    tmp["speak"] = Object.values(img)[i]["speak"];
-                    image_and_sound_dict[i] = tmp;
-                }
+	return (
+        <div className="quar__ctrl">
+			{
+                state === "moving" ?
+                    <div className="quar__mv">
+                        <div className="quar__title">Move To POI</div>
+                        <LottieControl loop={true} autoplay={true} data={Image.Moving} />
+                    </div>
+                :
+                    <div className="quar__sv">
+                        <ChangeImage timeout={5000} />
+                    </div>
             }
-        })
+        </div>
+    )
+}
 
-        safeApply($scope, function (bodyScope) {});
-    }
-
-    ui_apply = function() {
-        if (idx >= Object.keys(image_and_sound_dict).length) {
-            idx = 0;
-        }
-
-        let image_url = Object.values(image_and_sound_dict)[idx]["image"];
-        let speak_key = Object.values(image_and_sound_dict)[idx]["speak"].split(".")[0];
-        let image_tag = document.getElementById("protect_img");
-
-        // 이미지 전환
-        image_tag.setAttribute("src", "/contents/img/quarantine/" + image_url);
-        // 음성 발화
-        create_quarantine_speak(speak_key);
-
-        idx += 1;
-
-        // check timestamp
-        console.log("Now Time = ", new Date().toLocaleTimeString());
-        safeApply($scope, function (bodyScope) {});
-    }
-
-    $scope.onEndQuarantine = () => {
-        console.log("End Schedule Time and Call Clear Interval");
-
-        clearInterval(interval);
-        safeApply($scope, function (bodyScope) {});
-    }
-
-    $scope.onStartQuarantine = (msg) => {
-        let service = msg.service;
-        let state = msg.state;
-
-        if (service == 'quarantine') {
-            switch (state) {
-                case 'moving':
-                    console.log("move to quarantine station ...");
-                    $scope.quarantine_state = STATE_MOVING;
-    
-                    lottieloader('quarantine_mv_lottie', './contents/img/lottie/move/data.json', true, true);
-                    break;
-    
-                case 'quarantine':
-                    console.log("start quarantine service ...");
-                    $scope.quarantine_state = STATE_QUARANTINE;
-
-                    // setInterval에서 설정해준 time만큼의 공백을 채워줄 수 있기 위해서 선언
-                    let image_url = Object.values(image_and_sound_dict)[idx]["image"];
-                    let speak_key = Object.values(image_and_sound_dict)[idx]["speak"].split(".")[0];
-                    let image_tag = document.getElementById("protect_img");
-                    image_tag.setAttribute("src", "/contents/img/quarantine/" + image_url);
-                    create_quarantine_speak(speak_key);
-
-                    idx += 1;
-
-                    // check timestamp
-                    console.log("Now Time = ", new Date().toLocaleTimeString());
-
-                    interval = setInterval("ui_apply()", 10000);
-                    break;
-            }
-        }
-
-        safeApply($scope, function (bodyScope) {})
-    }
-})
+export default Quarantine;
