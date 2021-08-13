5f182ffc97cdb3aad5272158a69af7821f232d1c
mhji <battlesun99@gmail.com>
2021-08-13 16:41:27 +0900

    Feat: load files using by axios & change image

--- client/src/quarantine.js
+++ client/src/quarantine.js
@@ -1,44 +1,76 @@
 import "./Quarantine.css";
 import * as Image from "./Image";
+import Axios from "axios";
 import { useEffect, useState } from "react";
 import { LottieControl } from "./Common";
 
-function ChangeImage({timeout}) {
-    const [state, setState] = useState(Image.Protect0);
+function ChangeImage ({ delay, pageList }) {
+    const [image, setImage] = useState(Image.ProtectList[0]);
+    const useImage = pageList.filter(item => item["use"] === true);
+    let useImageBox = [];
 
-    useEffect (() => {
-        const pageList = [Image.Protect0, Image.Protect1, Image.Protect2];
+    Image.ProtectList.forEach(item => {
+        useImage.forEach((k, v) => {
+            if (item.split("/")[3].split(".")[0] === k.image.split(".")[0]) {
+                useImageBox.push(item)
+            }
+        })
+    });
+
+    useEffect(() => {
         let idx = 1;
-        const intervalID = setInterval(() => {
-            setState(pageList[idx])
-            idx = (idx + 1) % pageList.length;
-        }, timeout);
+        const intervalID = setInterval(async () => {
+            setImage(useImageBox[idx]);
+            idx = (idx + 1) % useImageBox.length;            
+        }, delay);
 
         return () => {
             clearInterval(intervalID);
         }
-    }, [])
-
+    }, []);
+    
     return (
-        <img src={state} alt="quarantine_img" />
+        <img src={image} alt="quarantine_img" />
     )
 }
 
-function Quarantine() {
-    const [state, setState] = useState("quaranting");
+function Quarantine ({ status }) {
+    const [delay, setDelay] = useState(1000);
+    const [pageList, setPageList] = useState({});
+    const [language, setLanguage] = useState([]);
 
+    useEffect(() => {
+        const Initializing = async () => {
+            const res_config = await Axios.get("./document/config.json");
+            const res_lang = await Axios.get("./document/language.json");
+    
+            if (res_config.status === 200 && res_lang.status === 200) {
+                setDelay(res_config.data.SPEAK_INTERVAL);
+                setPageList(res_config.data.IMAGE_SPEAK_ITEM);
+                setLanguage(res_lang.data.list);
+            }
+        }
+
+        // initializing config.json & language.json
+        Initializing()
+    }, [])
+    
 	return (
         <div className="quar__ctrl">
 			{
-                state === "moving" ?
-                    <div className="quar__mv">
-                        <div className="quar__title">Move To POI</div>
-                        <LottieControl loop={true} autoplay={true} data={Image.Moving} />
-                    </div>
+                Object.keys(pageList).length !== 0 && Object.keys(language).length !== 0 ?
+                    status === "moving" ?
+                        <div className="quar__mv">
+                            <div className="quar__title">{language["TEXT/QUARANTINE/MOVE_TITLE"]}</div>
+                            <div className="quar__title__sub">{language["TEXT/QUARANTINE/MOVE_SUB_TITLE"]}</div>
+                            <LottieControl loop={true} autoplay={true} data={Image.Moving} />
+                        </div>
+                    :
+                        <div className="quar__sv">
+                            <ChangeImage delay={delay} pageList={pageList} />
+                        </div>
                 :
-                    <div className="quar__sv">
-                        <ChangeImage timeout={5000} />
-                    </div>
+                    <>페이지 로딩 중</>
             }
         </div>
     )
