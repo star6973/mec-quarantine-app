25e88c266892291f3354691ea9b4c2e913187704
mhji <battlesun99@gmail.com>
2021-08-25 17:35:56 +0900

    Refactor: console component

--- client/src/console.js
+++ client/src/console.js
@@ -1,14 +1,7 @@
 import './Console.scss';
-import { useState, useEffect } from "react";
 import Keypad from './components/Keypad';
 
 function Console() {
-    const [keyList, setKeyList] = useState();
-    
-    useEffect(() => {
-
-    }, [])
-
     return (
         <Keypad />
     )
